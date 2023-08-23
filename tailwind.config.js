054e7992553b109059b6afcf0d8d6aefa8ba4df0
Alfin89 <rebahansantuy30@gmail.com>
2023-08-23 16:21:18 +0700

    Menarik file indek keluar from folder public

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "index.html"], // "./src/**/*.{html,js}"
+  content: ["index.html"], // "./src/**/*.{html,js}" "./public/**/*.{html,js}", 
   theme: {
     extend: {},
   },
